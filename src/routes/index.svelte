4663a3859165dd150fe3963119313f56d1c4885f
Steve <stevedsimkins@gmail.com>
2022-07-26 20:54:06 -0400

    added confetti

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,33 @@
 
 <script>
   import logo from "$lib/pinnie.svg";
+  import { ConfettiExplosion } from 'svelte-confetti-explosion'
+	import { tick } from 'svelte'
+  import { onMount } from 'svelte'
+	
+  let isVisible = false;
+  let isLoaded = false;
+
+  const handleClick = async e => {
+		isVisible = false;
+		await tick();
+    isVisible = true
+  }
+
+  onMount(async () => {
+    isVisible = false
+    await tick()
+    isVisible = true
+  })
 </script> 
 
+<svelte:body on:click={handleClick} />
+
+
 <div class="container">
+  {#if isVisible}
+    <ConfettiExplosion colors={['#8000DB', '#580098', '#98005E', '#FF009E', '#FFE001', '#FF9900', '#008298', '#00BEDD' ]} particlesShape="rectangles" force={0.7} />
+  {/if}
   <img src={logo} alt="Pinata Logo" />
   <h1>No</h1>
 </div>
