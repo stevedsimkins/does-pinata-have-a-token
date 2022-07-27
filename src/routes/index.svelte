0c11b0eefeb3006f40a901f165e051a273350583
Steve <stevedsimkins@gmail.com>
2022-07-26 20:37:53 -0400

    initial files and format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Mulish&display=swap")
+  </style>
+</svelte:head>
+
+<script>
+  import logo from "$lib/pinnie.svg";
+</script> 
+
+<div class="container">
+  <img src={logo} alt="Pinata Logo" />
+  <h1>No</h1>
+</div>
+
+<style>
+
+  .container {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }  
+
+  h1 {
+    font-family: "Mulish", sans-serif;
+    font-size: 90px;
+    font-weight: bold;
+  }
+
+</style>
